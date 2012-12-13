f8ed91a5abcbb498728bdc138987234fa1c7dc81
John Oliva <joliva63@gmail.com>
2012-12-13 13:13:49 -0500

    Add implementation for DB.list().

--- 250/Resources/lib/db.js
+++ 250/Resources/lib/db.js
@@ -1,4 +1,4 @@
-funtion DB() {
+function DB() {
 	//bootstrap the database
 	var db = Ti.Database.open('TiBountyHunter');	
 	db.execute('CREATE TABLE IF NOT EXISTS fugitives(id INTEGER PRIMARY KEY, name TEXT, captured INTEGER);');	
@@ -6,7 +6,22 @@ funtion DB() {
 }
 
 DB.prototype.list = function(isCaptured){
+	var results = [];	// array to hold query result as table rows
 	
+	var resultSet = db.execute('SELECT id, name FROM fugitives WHERE captured = ' + (isCaptured ? '1' : '0'));
+	
+	while (resultSet.isValidRow()) {
+		var row = Ti.UI.createTableViewRow({
+			id: resultSet.fieldByName('id'),
+			name: resultSet.fieldByName('name')
+		});
+		
+		results.push(row);
+		resultSet.next();
+	}
+	
+	resultSet.close();
+	return results;
 }
 
 DB.prototype.add = function(fugitiveName){
