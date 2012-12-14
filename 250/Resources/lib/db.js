c398077bbff2c86ad6f8a7aabcc878aa76a020d1
John Oliva <joliva63@gmail.com>
2012-12-14 06:39:04 -0500

    Additional comments.

--- 250/Resources/lib/db.js
+++ 250/Resources/lib/db.js
@@ -1,22 +1,27 @@
-funtion DB() {
+function DB() {
 	//bootstrap the database
 	var db = Ti.Database.open('TiBountyHunter');	
 	db.execute('CREATE TABLE IF NOT EXISTS fugitives(id INTEGER PRIMARY KEY, name TEXT, captured INTEGER);');	
 	db.close();
 }
 
+// Returns a set of table rows representing either captured or at-large fugitives. Each row should have title, id, 
+// hasChild=true, color='#fff', name (duplicate of title), and captured=boolean properties.
 DB.prototype.list = function(isCaptured){
 	
 }
 
+// Adds the named fugitive to the database. Fires an App-level event noting that the database has been updated.
 DB.prototype.add = function(fugitiveName){
 	
 }
 
+// Deletes the fugitive with the given ID. Fires the 'database updated' App-level event.
 DB.prototype.del = function(fugitiveId){
 	
 }
 
+// Marks the fugitive with the given ID as captured. Fires the 'database updated' App-level event.
 DB.prototype.bust = function(fugitiveId){
 	
 }
